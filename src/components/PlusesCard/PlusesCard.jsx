2454b9869ccc2d852757a2c7afa41dcda55211b0
Redison <redison.ray3318@gmail.com>
2025-01-28 17:35:26 +0300

    layout is finished

--- src/components/PlusesCard/PlusesCard.jsx
+++ src/components/PlusesCard/PlusesCard.jsx
@@ -2,6 +2,7 @@ import './PlusesCard.scss';
 function PlusesCard(props) {
     return (
         <article className="pluses__card">
+            <span className="stats-card__icon" dangerouslySetInnerHTML={{ __html: props.img }} />
             <h3>{props.name}</h3>
             <p>{props.text}</p>
         </article>
